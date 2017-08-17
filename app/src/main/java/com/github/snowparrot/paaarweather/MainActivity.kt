ead5621ffb17cc90f912ef5501627871f26c27ab
snowparrot <snowparrot@live.com>
2017-08-17 09:33:59 +0200

    add first view + new layout

--- app/src/main/java/com/github/snowparrot/paaarweather/MainActivity.kt
+++ app/src/main/java/com/github/snowparrot/paaarweather/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.github.snowparrot.paaarweather
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
