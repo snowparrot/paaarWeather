7af836d15366abd56a3d88fc377d977f01974559
snowparrot <snowparrot@live.com>
2017-09-08 16:53:55 +0200

    better UI

--- app/src/main/java/com/github/snowparrot/paaarweather/WeatherView.kt
+++ app/src/main/java/com/github/snowparrot/paaarweather/WeatherView.kt
@@ -9,6 +9,7 @@ import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.github.snowparrot.paaarweather.symbols.*
 
@@ -19,7 +20,9 @@ class WeatherView : Activity() {
     lateinit var temperature: String
 
     lateinit var textLocation: TextView
+    lateinit var weatherImage: ImageView
     lateinit var iconWeather: weatherIcons
+    lateinit var textTemperature: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(16974382) // Theme_Material_NoActionBar
@@ -27,6 +30,9 @@ class WeatherView : Activity() {
         setContentView(R.layout.weatherview)
 
         textLocation = findViewById(R.id.textLocation)
+        weatherImage = findViewById(R.id.weatherImage)
+        textTemperature = findViewById(R.id.textTemperature)
+
         var b = intent
         location = b.getStringExtra("location")
         temperature = b.getStringExtra("temperature")
@@ -34,10 +40,22 @@ class WeatherView : Activity() {
         iconWeather = parseStringToWeatherIcons(b.getStringExtra("symbol"))
 
         textLocation.setText(location)
+        weatherImage.setImageResource(getImageByweatherIcon(iconWeather))
+        textTemperature.setText(temperature)
 
 
 
+    }
 
 
+    fun getImageByweatherIcon (icon: weatherIcons) : Int {
+        when (icon) {
+            weatherIcons.DAY_SUN -> return(R.drawable.sonne)
+            weatherIcons.DAY_RAIN -> return(R.drawable.regen)
+            weatherIcons.NIGHT_RAIN -> return(R.drawable.regen)
+            weatherIcons.DAY_MIST -> return(R.drawable.nebel)
+            weatherIcons.NIGHT_MIST -> return(R.drawable.nebel)
+            else -> return(R.drawable.nebel)
+        }
     }
 }
